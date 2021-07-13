aeda1e869b914ca01e93c0fcc335941ac06d613e
mohammad <m.a.taghva@gmail.com>
2021-07-12 23:25:11 -0700

    Add error message and focus data

--- src/js/main.js
+++ src/js/main.js
@@ -19,10 +19,10 @@ yDomainElement.addEventListener('input', (e) => {
     updateData();
 });
 
-let zeroCheckElement = document.getElementById("zeroCheck");
-let excludeZero = zeroCheckElement.checked;
-zeroCheckElement.addEventListener('click', (e) => {
-    excludeZero = e.target.checked;
+let posCheckElement = document.getElementById("posCheck");
+let excludePos = posCheckElement.checked;
+posCheckElement.addEventListener('click', (e) => {
+    excludePos = e.target.checked;
     updateData()
 });
 
@@ -52,20 +52,24 @@ let exPointsArr = validateExPoints(exPoints);
 
 let funcElement = document.getElementById("function");
 let func = funcElement.value;
-let validFunc;
+let errElement = document.getElementById("errMessage");
+let [validFunc, errorMessage] = validate(func);
+errElement.innerText = errorMessage;
+
 funcElement.addEventListener('input', (e) => {
     func = e.target.value;
-    validFunc = validate(func);
-    if (validFunc.includes("log")) {
-        zeroCheckElement.checked = true;
-        excludeZero = true;
-        negCheckElement.checked = true;
-        excludeNeg = true;
-    }
+    [validFunc, errorMessage] = validate(func);
+    errElement.innerText = errorMessage;
+    // if (validFunc.includes("log")) {
+    //     zeroCheckElement.checked = true;
+    //     excludeZero = true;
+    //     negCheckElement.checked = true;
+    //     excludeNeg = true;
+    // }
     updateData();
 });
 
-validFunc = validate(func);
+// validFunc = validate(func);
 
 let svg = d3.select("#graph").append("svg")
 .attr("class", "svg")
@@ -84,13 +88,15 @@ let yScale = d3.scaleLinear()
        .domain([-yDomain, yDomain])
        .range([h - padding, padding]);
 
-for (let i = -200; i < 200; i++) {
-    if (excludeZero && i === 0) continue;
-    if (excludeNeg && i < 0) continue;
-    x = (i/200) * xDomain;
+for (let i = -250; i < 250; i++) {
+    x = (i/250) * xDomain;
     y = eval(validFunc);
-    if (exPointsArr.includes(x)) continue;
-    // if ((y > yDomain) || (y < -yDomain)) continue;
+    if (y >= (2 * yDomain) || 
+        y <= -(2 * yDomain) ||
+        (excludePos && i > 0) || 
+        (excludeNeg && i < 0) ||
+        (exPointsArr.includes(x)))
+        {y = null};
     dataset.push([x, y])
 };
 
@@ -116,27 +122,115 @@ svg.append("text")
     .attr("y", 12)
     .text("y")
 
-
-var lineFunction = d3.line()
-    .x((d) => d[0])
-    .y((d) => d[1])
+let lineFunction = d3.line()
+        .defined(function (d) {return d[1] !== null;})
+    // .x((d) => d[0])
+    // .y((d) => d[1])
     // .curve(d3.curveCatmullRom.alpha(0.5));
     // .curve(d3.curveStep);
     // .curve(d3.curveCardinal.tension(0.5));
     // .curve(d3.curveLinear);
-    .curve(d3.curveBasisOpen);
+    // .curve(d3.curveBasisOpen);
 
 
-var lineData = [];
+let lineData = [];
 for (let i = 0; i < dataset.length; i++) {
-    lineData.push([xScale(dataset[i][0]), yScale(dataset[i][1])]);
+    lineData.push([xScale(dataset[i][0]),
+                    (dataset[i][1] === null) || isNaN(dataset[i][1]) ? null : yScale(dataset[i][1])]);
 };
-var lineGraph = svg.append("g")
+let lineGraph = svg.append("g")
                     .append("path")
                     .attr("d", lineFunction(lineData))
-                    .attr("stroke-width", 3)
+                    .attr("stroke-width", 2)
                     .attr("stroke", graphColor)
                     .attr("fill", "none");
+
+let focus = svg
+            .append("g")
+            .append("circle")
+            .style("fill", "red")
+            .attr("stroke", "black")
+            .attr("r", 4)
+            .style("opacity", 0);
+
+let bisect = d3.bisector(function(d) {return d[0]}).left;
+
+let focusText = svg
+                .append("g")
+
+let focusTextX = focusText
+                .append("text")
+                .style("opacity", 0)
+                .attr("class", "focusDataLabel")
+                .attr("text-anchor", "left")
+                .attr("alignment-baseline", "middle")
+
+let focusTextY = focusText
+                .append("text")
+                .style("opacity", 0)
+                .attr("class", "focusDataLabel")
+                .attr("text-anchor", "left")
+                .attr("alignment-baseline", "middle")
+
+svg
+    .append("rect")
+    .style("fill", "none")
+    .style("pointer-events", "all")
+    .attr("width", w)
+    .attr("height", h)
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseout", mouseout);
+
+function mouseover() {
+    focus.style("opacity", 1)
+    focusTextX.style("opacity", 1)
+    focusTextY.style("opacity", 1)
+};
+
+function mousemove(event) {
+    let x0 =xScale.invert(d3.pointer(event)[0]);
+    let i = bisect(dataset, x0, 1);
+    let selectedData = dataset[i];
+    let xOffset = 10;
+    let yOffset = 0;
+    if (selectedData[1] === null ||
+        yScale(selectedData[1]) < 0 ||
+        yScale(selectedData[1]) > h) {
+        focus.style("opacity", 0)
+        focusTextX.style("opacity", 0)
+        focusTextY.style("opacity", 0)
+    } else {
+        if (xScale(selectedData[0]) > w - 85) {xOffset = -70;};
+        if (yScale(selectedData[1]) < 15) {yOffset = 10;};
+        if (yScale(selectedData[1]) > h - 30) {yOffset = -30;};
+        focus
+            .style("opacity", 1)
+            .attr("cx", (xScale(selectedData[0])))
+            .attr("cy", (yScale(selectedData[1])));
+        focusTextX
+            .style("opacity", 1)
+            .html("x: " + Math.round(selectedData[0] * 1000)/1000)
+            .attr("x", xScale(selectedData[0]) + xOffset+ 10)
+            .attr("y", yScale(selectedData[1]) + yOffset - 7);
+        
+        focusTextY
+            .style("opacity", 1)
+            .html("y: " + Math.round(selectedData[1] * 1000)/1000)
+            .attr("x", xScale(selectedData[0]) + xOffset + 10)
+            .attr("y", yScale(selectedData[1]) + yOffset + 8)
+    }
+    
+}   
+
+function mouseout() {
+    focus.style("opacity", 0)
+    focusTextX.style("opacity", 0)
+    focusTextY.style("opacity", 0)
+}
+
+
+
 
 function updateData() {
 
@@ -151,24 +245,30 @@ function updateData() {
         .duration(750)
         .call(d3.axisLeft(yScale));   
 
-    let newDataset = [];
+    dataset = [];
     for (let i = -200; i < 200; i++) {
-        if (excludeZero && i === 0) continue;
-        if (excludeNeg && i < 0) continue;
         x = (i/200) * xDomain;
         y = eval(validFunc);
-        if (exPointsArr.includes(x)) continue;
-        // if ((y > yDomain) || (y < -yDomain)) continue;
-        newDataset.push([x, y])
+        if (y >= (2 * yDomain) || 
+            y <= -(2 * yDomain) ||
+            (excludePos && i > 0) || 
+            (excludeNeg && i < 0) ||
+            (exPointsArr.includes(x)))
+            {y = null};
+        dataset.push([x, y])
+    };
+    console.log("newDataSet   ", dataset)
+    lineData = []
+    for (let i = 0; i < dataset.length; i++) {
+            lineData.push([xScale(dataset[i][0]), 
+                            (dataset[i][1] === null) || isNaN(dataset[i][1]) ? null : yScale(dataset[i][1])]);
     };
 
-    let newLineData = []
-    for (let i = 0; i < newDataset.length; i++) {
-            newLineData.push([xScale(newDataset[i][0]), yScale(newDataset[i][1])]);
-    };
     lineGraph
         .transition()
         .duration(500)
-        .attr("d", lineFunction(newLineData))
+        .attr("d", lineFunction(lineData))
         .attr("stroke", graphColor);
+
 };
+
--- src/js/validate.js
+++ src/js/validate.js
@@ -2,18 +2,32 @@
 const mathFuncs = ["asin(", "sinh(", "sin(", "acos(", "cosh(", "cos(", "atan(", "tanh(", "tan(", "log(", "log10(", "exp(", "abs("];
 
 function validate (func) {
-        let validFunc;
-        let reg;
-        RegExp.quote = function(str) {
-            return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
-        };
-        func = func.toLowerCase().replace(/math.|delete|drop|copy|format|edit|destroy|register|get|post|put|new/g, "");
-        for (let i = 0; i < mathFuncs.length; i++) {
-            reg = new RegExp(RegExp.quote(mathFuncs[i]), "g");
-            func = func.replace(reg, `Math.${mathFuncs[i].toUpperCase()}`);
-        };
+    let validFunc;
+    let reg;
+    let errorMessage = "";
+    RegExp.quote = function(str) {
+        return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+    };
+    func = func.toLowerCase().replace(/math.|delete|drop|copy|format|edit|destroy|register|get|post|put|new/g, "");
+    for (let i = 0; i < mathFuncs.length; i++) {
+        reg = new RegExp(RegExp.quote(mathFuncs[i]), "g");
+        func = func.replace(reg, `Math.${mathFuncs[i].toUpperCase()}`);
+    };
     validFunc = func.toLowerCase().replace(/math/g, "Math")
-    return validFunc;
+    let balParentheses = 0;
+    for (let i=0; i < validFunc.length; i++) {
+        if (validFunc[i] === "(") balParentheses += 1;
+        if (validFunc[i] === ")") balParentheses -= 1;
+        if (balParentheses < 0) {
+            errorMessage = "Paretheses are not in order"
+            return [validFunc, errorMessage]
+        }
+    }
+    if (balParentheses > 0) {
+        errorMessage = "Paretheses are not balanced"
+    }
+
+    return [validFunc, errorMessage];
 };
 
 function validateExPoints(exPoints) {
